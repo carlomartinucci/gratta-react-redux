c1c2524b9d02a59111f5a6648874da7b7553218c
Ch <carlo.martinucci@gmail.com>
2017-04-16 19:40:01 +0200

    transition on Credit-current

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -5,21 +5,33 @@ import './Credit.css'
 
 const Credit = ({creditID, current, lastCost, lastWin}) => (
   <div className="Credit">
-    <div className="Credit-current">{current} €</div>
+    <CSSTransitionGroup
+      transitionName="Credit-current"
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={500}>
+      <div key={creditID} className="Credit-current">
+        {current} €
+      </div>
+    </CSSTransitionGroup>
+    
     <div className="Credit-last">
       <CSSTransitionGroup
         style={{position: 'relative', width: '100%'}}
         transitionName="Credit-action"
         transitionEnterTimeout={1000}
         transitionLeaveTimeout={0}>
-        <div key={creditID} className="Credit-cost Credit-action">- {lastCost} €</div>
+        <div key={creditID} className="Credit-cost Credit-action">
+          - {lastCost} €
+        </div>
       </CSSTransitionGroup>
       <CSSTransitionGroup
         style={{position: 'relative', width: '100%'}}
         transitionName="Credit-action"
         transitionEnterTimeout={1000}
         transitionLeaveTimeout={0}>
-        <div key={creditID} className="Credit-win Credit-action">+ {lastWin} €</div>
+        <div key={creditID} className="Credit-win Credit-action">
+          + {lastWin} €
+        </div>
       </CSSTransitionGroup>
     </div>
   </div>
